6066cc596dd5d56754a125b72b1ab926931e0068
anupchand007 <2078bit@gmail.com>
2025-02-17 17:18:27 +0545

    content updates: inheritance and polymorphism

--- JS_Scripts/class.js
+++ JS_Scripts/class.js
@@ -1,32 +1,48 @@
-class Person{
-    constructor(name, age){
+class Animal {
+    setData(name, type) {
+        this.a = name;
+        this.b = type;
+    }
+
+    showData() {
+        console.log(`The Animal name is ${this.a}, while it is a ${this.b} type of animal`);
+    }
+}
+
+let cat = new Animal();                         //creating a new object from the parent class
+cat.setData("Bubbly", "Gharelu");
+cat.showData();
+
+let tiger = new Animal();                       //creating a new object called tiger
+tiger.setData("Jumbo", "Wild");
+tiger.showData();
+
+
+class Person {
+    constructor(name, age) {
         this.name = name;
         this.age = age;
     }
 
-    greet(){
+    greet() {
         console.log(`This is my name ${this.name}`);
     }
+
+    
 }
 
 let anup = new Person("Anup", 22);
 anup.greet();
 
-class Animal{
-    setData(name, type){
-        this.a = name;
-        this.b = type;
-    }
+//Inheritance
 
-    showData(){
-        console.log(`The Animal name is ${this.a}, while it is a ${this.b} type of animal`);
+class Anup extends Person {
+    greeter() {
+        console.log("This is a greet msg form Anup class which inherits properties from Class Person");
     }
 }
 
-let cat = new Animal();
-cat.setData("Bubbly", "Gharelu");
-cat.showData();
+let p1 = new Anup("Anurag", 22);
+p1.greet();                         // anup does not have greet() method but it inherits from class person
+p1.greeter();          
 
-let tiger = new Animal();
-tiger.setData("Jumbo", "Wild");
-tiger.showData();
