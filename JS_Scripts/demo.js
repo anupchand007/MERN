96240170c7c0889d01ca19a6fb26d00417f352d4
anupchand007 <2078bit@gmail.com>
2025-02-17 11:55:24 +0545

    collage

--- JS_Scripts/demo.js
+++ JS_Scripts/demo.js
@@ -1,30 +1,52 @@
-document.getElementsByTagName("input").style.color = "red";
+// document.getElementsByTagName("input").style.color = "red";
 
-function Validator(){
-    let name = document.getElementById("name").value;
-    let age = document.getElementById("age").value;
-    let address = document.getElementById("address").value;
-    let err = document.getElementById("err");
-    err.style.fontSize = "larger";
-    
+// function Validator(){
+//     let name = document.getElementById("name").value;
+//     let age = document.getElementById("age").value;
+//     let address = document.getElementById("address").value;
+//     let err = document.getElementById("err");
+//     err.style.fontSize = "larger";
 
-    err.innerText = "";
 
-    if (name === "" || age === "" || address === ""){
-        err.innerHTML = "Please fill all the Fields";
-        return false;
-    }
+//     err.innerText = "";
 
-    if(name.length < 3){
-        err.innerText = "Name is too short";
-        return false;
-    }
-    if(age < 18){
-        err.innerText = "User must be 18 years old";
-        return false;
-    }
-    return true;
-    
+//     if (name === "" || age === "" || address === ""){
+//         err.innerHTML = "Please fill all the Fields";
+//         return false;
+//     }
+
+//     if(name.length < 3){
+//         err.innerText = "Name is too short";
+//         return false;
+//     }
+//     if(age < 18){
+//         err.innerText = "User must be 18 years old";
+//         return false;
+//     }
+//     return true;
+
+// }
+
+// console.log("Done Compiling"
+
+
+let x = document.getElementById("num1").value;
+let y = document.getElementById("num2").value;
+
+let result = document.getElementById("disp");
+result.innerText = "";
+
+function sum() {
+    result.innerText = x + y;
+}
+function div() {
+    result.innerText = x / y;
+}
+function sub() {
+    result.innerText = x - y;
+}
+function mul() {
+    result.innerText = x * y;
 }
 
-console.log("Done Compiling")
+console.log("weee");
