ab00405400fbe0a86af4d15886d1957ed23ce3e1
anupchand007 <2078bit@gmail.com>
2025-04-12 20:38:21 +0545

    Basic Understand

---
+++ JavaScript/01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const userName = "Anup Chand";
+let userID = 1234;              //widely used n always use lET;
+var Age = 22
+userCity = "KTM"                //do not ever use such deceleration
+let myDefence
+
+
+console.log(userName);
+
+console.table([userName, userID, Age, userCity, myDefence]);
---
+++ JavaScript/01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" //treat all JS code as newer version
+//but latest version of JS automatically runs in new version
+
+// console.log(2 +4);
+
+let result = console.log(2 + 4);
+
+//alert("Hellow World!!")
+
+let  name  = "Anup Chand"
+let age = 22
+let gender = "male"
+let loggedIn = true
+
+// number => 2^53
+// bigint
+// string => "" //use in double quote
+// boolean => true or false
+// null => standalone value/ represent empty value
+// undefined => when a value is not defined
+// symbol => unique
+
+
+// object
+
+console.log(typeof (undefined));        //undefined type
+console.log(typeof (null));             // onbjects
