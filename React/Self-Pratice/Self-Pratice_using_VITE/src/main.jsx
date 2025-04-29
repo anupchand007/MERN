5b610a0b6adf404592de1451e6cf266d49b0014c
anupchand07 <2078bit@gmail.com>
2025-04-29 15:52:22 +0545

    Random Password Generator

--- React/Self-Pratice/Self-Pratice_using_VITE/src/main.jsx
+++ React/Self-Pratice/Self-Pratice_using_VITE/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Counter from './App.jsx'
 import SwitchColor from './App.jsx'
+import RandomPassword from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 
     // <Counter />
-    <SwitchColor />
+    // <SwitchColor />
+    <RandomPassword />
 
 )
