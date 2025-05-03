273c9631dac9d532d3c8101b89b3b600d3ca6a0c
anupchand07 <2078bit@gmail.com>
2025-05-03 20:48:02 +0545

    Todo App

---
+++ React/10todocontextLocal/src/Context/TodoContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useContext } from "react";
+
+const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "Todo msg",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {}
+});
+
+export default TodoContext;
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
+
+export const TodoProvide = TodoContext.Provider;
