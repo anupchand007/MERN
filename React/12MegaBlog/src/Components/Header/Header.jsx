d821e538dd5413e5a0b0cb3ffef98bdb38cb8bb8
anupchand007 <2078bit@gmail.com>
2025-05-22 20:46:24 +0545

    Components added

--- React/12MegaBlog/src/Components/Header/Header.jsx
+++ React/12MegaBlog/src/Components/Header/Header.jsx
@@ -1,10 +1,70 @@
 import React from 'react'
+import {Container, Logo, LogoutBtn}  from "../index"
+import { Link, Navigate, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 
 const Header = () => {
+  const authStatus  = useSelector((state) => state.auth.status)
+
+  const navItems = useNavigate([
+    {
+      name: "Home",
+      url: "/home",
+      active: authStatus
+    },
+    {
+      name: "Login",
+      url: "/login",
+      active: !authStatus
+    },
+    {
+      name: "Signup",
+      url: "/signup",
+      active: !authStatus
+    },
+    {
+      name: "All Post",
+      url: "/all-posts",
+      active: authStatus
+    },
+    {
+      name: "Add Post",
+      url: "/add-posts",
+      active: authStatus
+    },
+  ])
+
   return (
-    <div>
-      Header
-    </div>
+    <header className='py-3 shadow bg-gray-500'>
+    <Container>
+      <nav className='flex'>
+        <div className='mr-4 '>
+          <Link to="/">
+          <Logo width='70px'/ >
+          </Link>
+        </div>
+          <ul className='flex ml-auto'>
+        {navItems.map((item) => (
+            item.active ? (
+              <li key={item.name}>
+                <button
+                onClick={() => Navigate(item.url)}
+                className='"inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                >{item.name}</button>
+              </li>
+            ) : null
+        ))}
+        {authStatus && (
+          <li>
+            <LogoutBtn/>
+          </li>
+        )}
+          </ul>
+      </nav>
+    </Container>
+
+    </header>
   )
 }
 
---
+++ React/12MegaBlog/src/Components/container/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Container = ({childern}) => {
+  return (
+    <div>
+        <div className='w-full max-w-7xl mx-auto px04'>
+            {childern}
+        </div>
+    </div>
+  )
+}
+
+export default Container
